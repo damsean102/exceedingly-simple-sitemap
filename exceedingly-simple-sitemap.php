c98698ce97fc66f54af7a1ea002dab8693c126c9
Sam Dean <damsean102@gmail.com>
2016-04-08 22:45:40 +0100

    Change to OOP style - Create class and move functions in

--- exceedingly-simple-sitemap.php
+++ exceedingly-simple-sitemap.php
@@ -20,118 +20,87 @@
  */
 
 
-/*
-* ADMIN FUNCTIONS - Create and save metabox information
-*/
+class ess_sitemap {
 
-function ess_metabox_markup($object) {
+	/*
+	* ADMIN FUNCTIONS - Create and save metabox information
+	*/
+	
+	function __construct(){
+		//Create Admin metabox
+		add_action("add_meta_boxes", array($this, "create_ess_metabox"));
+		//When post is saved, save the meta data
+		add_action("save_post", array($this, "save_ess_metabox"), 10, 3);
 
-	wp_nonce_field(basename(__FILE__), "ess-metabox-nonce");
-
-	$checkboxVal = get_post_meta($object->ID, "ess-checkbox", true); ?>
-
-		<fieldset>
-			<legend class="screen-reader-text"><span>Exclude this page from the sitemap?</span></legend>
-			<label for="exclude-sitemap-checkbox">
-				<input id="exclude-sitemap-checkbox" name="ess-checkbox" type="checkbox" value="true" <?php checked( $checkboxVal, 'true', TRUE ); ?>>
-				<span><?php esc_attr_e('Yes'); ?></span>
-			</label>
-		</fieldset>
-
-	<?php
-}
-
-function create_ess_metabox() {
-	 add_meta_box("ess-simple-sitemap", "Exclude from sitemap?", "ess_metabox_markup", "page", "side", "default", null);
-}
-
-add_action("add_meta_boxes", "create_ess_metabox");
+		//Create the Shotcode
+		add_shortcode('ess-sitemap', array($this, 'ess_html_sitemap'));
+		//Create XML sitemap
+		add_action('init', array($this,'ess_xml_sitemap'));
+	}
 
 
-function save_ess_metabox($post_id, $post, $update){
-	//If nonce field cannot be verfied or isn't set
-	if (!isset($_POST["ess-metabox-nonce"]) || !wp_verify_nonce($_POST["ess-metabox-nonce"], basename(__FILE__)))
-		return $post_id;
+	/*
+	* ADMIN FUNCTIONS - Create and save metabox information
+	*/
 
-	//If the user can't edit the post/page
-	if(!current_user_can("edit_posts", $post_id) || !current_user_can("edit_pages", $post_id))
-		return $post_id;
+	function ess_metabox_markup($object) {
 
-	//If the post is doing an autosave
-	if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
-		return $post_id;
+		wp_nonce_field(basename(__FILE__), "ess-metabox-nonce");
 
-	$slug = "page";
-	//If the post type is page
-	if($slug != $post->post_type)
-		return $post_id;
+		$checkboxVal = get_post_meta($object->ID, "ess-checkbox", true); ?>
 
-	$essCheckboxVal = "";
+			<fieldset>
+				<legend class="screen-reader-text"><span>Exclude this page from the sitemap?</span></legend>
+				<label for="exclude-sitemap-checkbox">
+					<input id="exclude-sitemap-checkbox" name="ess-checkbox" type="checkbox" value="true" <?php checked( $checkboxVal, 'true', TRUE ); ?>>
+					<span><?php esc_attr_e('Yes'); ?></span>
+				</label>
+			</fieldset>
 
-	if(isset($_POST["ess-checkbox"])) {
-		$essCheckboxVal = $_POST["ess-checkbox"];
-	}   
-	update_post_meta($post_id, "ess-checkbox", $essCheckboxVal);
-}
+		<?php
+	}
 
-add_action("save_post", "save_ess_metabox", 10, 3);
+	function create_ess_metabox() {
+		 add_meta_box("ess-simple-sitemap", "Exclude from sitemap?", array($this, "ess_metabox_markup"), "page", "side", "default", null);
+	}
 
 
-/*
-* GENERATE SITEMAPS
-*/
+	function save_ess_metabox($post_id, $post, $update){
+		//If nonce field cannot be verfied or isn't set
+		if (!isset($_POST["ess-metabox-nonce"]) || !wp_verify_nonce($_POST["ess-metabox-nonce"], basename(__FILE__)))
+			return $post_id;
+
+		//If the user can't edit the post/page
+		if(!current_user_can("edit_posts", $post_id) || !current_user_can("edit_pages", $post_id))
+			return $post_id;
+
+		//If the post is doing an autosave
+		if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+			return $post_id;
+
+		$slug = "page";
+		//If the post type is page
+		if($slug != $post->post_type)
+			return $post_id;
+
+		$essCheckboxVal = "";
+
+		if(isset($_POST["ess-checkbox"])) {
+			$essCheckboxVal = $_POST["ess-checkbox"];
+		}   
+		update_post_meta($post_id, "ess-checkbox", $essCheckboxVal);
+	}
 
 
-//Create a shortcode for HTML sitemap
-function ess_html_sitemap() {
-	global $post;
+	/*
+	* GENERATE SITEMAPS
+	*/
 
 	// Build list of pages where 'ess-checkbox' metabox doesn't equal true
-	$args = array(
-		'posts_per_page'  => -1,
-		'post_type'				=> array('page'),
-		'post_status'			=> 'publish',
-		'title_li'  			=> '',
-		'orderby' 				=> 'menu_order, post_title',
-		'order' 					=> 'ASC',
-		'meta_key'				=> 'ess-checkbox',
-		'meta_value'			=> 'true',
-		'meta_compare'		=> '!='
-	);
-
-	$allPosts = get_posts( $args );
-
-	$html = '';
-
-	if ($allPosts):
-
-		$html = "<div class='ess-sitemap'><ul>";
-		foreach ($allPosts as $post):
-
-			$html .= "<li><a href='" . get_permalink($post->ID) . "'>" . get_the_title($post->ID) . "</a></li>";
-
-		endforeach;
-		$html .= "</ul></div>";
+	function ess_get_all_posts() {
 		
-	endif;
-
-
-	return $html;
-
-}
-
-add_shortcode('ess-sitemap', 'ess_html_sitemap');
-
-
-//Create XML sitemap - http://www.sitemaps.org/protocol.html
-
-add_action( 'save_post', 'ess_xml_sitemap' );
-function ess_xml_sitemap() {
 		global $post;
 
-		$sitemapURL = ABSPATH . 'sitemap.xml';
-
-		// Build list of pages where 'ess-checkbox' metabox doesn't equal true
 		$args = array(
 			'posts_per_page'  => -1,
 			'post_type'				=> array('page'),
@@ -144,9 +113,64 @@ function ess_xml_sitemap() {
 			'meta_compare'		=> '!='
 		);
 
-		$allPosts = get_posts( $args );
+		$allPosts = get_posts($args);
 
-    if ($allPosts):
+		if ($allPosts):
+
+			$allPostArray = array();
+
+			foreach ($allPosts as $post):
+
+				$title = get_the_title($post->ID);
+				$locValue = get_permalink($post->ID);
+				$lastModValue = get_the_modified_date('Y-m-d');
+				
+				$allPostArray[$post->ID] = array(
+					'title' => $title,
+					'permalink' => $locValue,
+					'last_mod_date' => $lastModValue
+				);
+
+			endforeach;
+			wp_reset_postdata();
+
+			return $allPostArray;
+
+		endif;
+
+	}
+
+	//$posts = ess_get_all_posts();
+
+	//Create a shortcode for HTML sitemap
+	function ess_html_sitemap($posts) {
+		
+		$allPosts = $this->ess_get_all_posts();
+
+		if ($allPosts):
+
+			$html = "<div class='ess-sitemap'><ul>";
+			foreach ($allPosts as $key=>$value):
+
+				$html .= "<li><a href='" . $value['permalink'] . "'>" . $value['title'] . "</a></li>";
+
+			endforeach;
+			$html .= "</ul></div>";
+			
+		endif;
+
+		return $html;
+
+	}
+
+	//Create XML sitemap - http://www.sitemaps.org/protocol.html
+	function ess_xml_sitemap() {
+
+		$sitemapURL = ABSPATH . 'sitemap.xml';
+
+		$allPosts = $this->ess_get_all_posts();
+		
+	  if ($allPosts):
 
 	    $sitemap = new DOMDocument("1.0", "UTF-8");
 
@@ -158,11 +182,11 @@ function ess_xml_sitemap() {
 			$urlset->setAttribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
 
 			//START LOOP HERE
-			foreach ($allPosts as $post):
+			foreach ($allPosts as $key=>$value):
 
 				//Get Values for Elements
-				$locValue = get_permalink($post->ID);
-				$lastModValue = get_the_modified_date('Y-m-d');
+				$locValue = $value['permalink'];
+				$lastModValue = $value['last_mod_date'];
 				$priorityValue = '0.5';
 
 				//Create a Elements
@@ -191,4 +215,13 @@ function ess_xml_sitemap() {
 
 		endif; //allposts
 
-}
+	}
+
+
+
+} //END CLASS
+
+
+
+//Initiate class
+$essSitemap = new ess_sitemap();
